10ff5f48520bb8d0b1d8fe56058e602165b05f2b
Jae <daniel.kwong.h@gmail.com>
2020-07-19 22:27:49 -0700

    update model for view

--- models/Diamond1.js
+++ models/Diamond1.js
@@ -5,17 +5,17 @@ const {Schema} = mongoose;
 
 const productSchema = new Schema({
     summonerName: String,
-    // summonerId: String,
-    // leaguePoints: String,
-    // rank: String,
-    // wins: String,
-    // losses: String,
-    // veteran: Boolean,
-    // inactive: Boolean,
-    // freshBlood: Boolean,
-    // hotStreak: Boolean,
-    // tier: String,
-    // Winrate: String
+    summonerId: String,
+    leaguePoints: String,
+    rank: String,
+    wins: String,
+    losses: String,
+    veteran: Boolean,
+    inactive: Boolean,
+    freshBlood: Boolean,
+    hotStreak: Boolean,
+    tier: String,
+    Winrate: String
 })
 
 mongoose.model('diamond1s', productSchema);
