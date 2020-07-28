7d94afedc575066e46170bf5a830af730e3d670f
Jae <daniel.kwong.h@gmail.com>
2020-07-27 23:21:42 -0700

    adds react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,68 +1,23 @@
-import React, { useState, useEffect } from "react";
-import {
-  MDBContainer,
-  MDBRow,
-  MDBCol,
-  MDBInput,
-  MDBBtn,
-  MDBCard,
-  MDBCardBody
-} from "mdbreact";
+import React from 'react';
+import './App.css';
+import {Route, BrowserRouter as Router, Switch} from "react-router-dom";
+import Home from "./pages/Home";
+import Diamond from "./pages/Diamond";
 
-// SERVICES
-import productService from './services/diamond1Service';
-//import productService from './services/playersService';
 
 function App() {
-  const [products, setproducts] = useState(null);
+  return(
+    <Router>
+      <div className="container">
+        <p>Reee</p>
+      </div>
 
-  useEffect(() => {
-    if(!products) {
-      getProducts();
-    }
-  })
-
-  const getProducts = async () => {
-    let res = await productService.getAll();
-    console.log(res);
-    setproducts(res);
-  }
-
-  const renderProduct = product => {
-    return (
-      <li key={product._id} className="list__item product">
-        <h3 className="product__name"><b>{product.summonerName}</b></h3>
-        {/* <p className="product__description">{product.summonerId}</p> */}
-        <p><mark>{product.tier} {product.rank}</mark></p>
-        <p>{product.leaguePoints} LP</p>
-        <p>{product.wins}W {product.losses}L</p>
-        <p>Winrate: {product.Winrate}</p>
-        <br></br>
-      </li>
-    );
-  };
-
-  return (
-    <MDBContainer>
-    <div className="App" >
-      <center>     
-        <MDBCard>
-          <br></br>
-        <h1>LoL NA Ladder Rankings</h1>
-        <br>
-        </br>
-      <ul className="list">
-        {(products && products.length > 0) ? (
-          products.map(product => renderProduct(product))
-        ) : (
-          <p>No products found</p>
-        )}
-      </ul>
-      </MDBCard>
-      </center>
-    </div>
-    </MDBContainer>
-  );
+      <Switch>
+      <Route path="/" exact component={Home}/>
+      <Route path="/diamond" component={Diamond}/>
+      </Switch>
+    </Router>
+  )
 }
 
 export default App;
---
+++ client/src/pages/Diamond.js
@@ -0,0 +1,68 @@
+import React, { useState, useEffect } from "react";
+import {
+  MDBContainer,
+  MDBRow,
+  MDBCol,
+  MDBInput,
+  MDBBtn,
+  MDBCard,
+  MDBCardBody
+} from "mdbreact";
+
+// SERVICES
+import productService from '../services/diamond1Service';
+//import productService from './services/playersService';
+
+function Diamond() {
+  const [products, setproducts] = useState(null);
+
+  useEffect(() => {
+    if(!products) {
+      getProducts();
+    }
+  })
+
+  const getProducts = async () => {
+    let res = await productService.getAll();
+    console.log(res);
+    setproducts(res);
+  }
+
+  const renderProduct = product => {
+    return (
+      <li key={product._id} className="list__item product">
+        <h3 className="product__name"><b>{product.summonerName}</b></h3>
+        {/* <p className="product__description">{product.summonerId}</p> */}
+        <p><mark>{product.tier} {product.rank}</mark></p>
+        <p>{product.leaguePoints} LP</p>
+        <p>{product.wins}W {product.losses}L</p>
+        <p>Winrate: {product.Winrate}</p>
+        <br></br>
+      </li>
+    );
+  };
+
+  return (
+    <MDBContainer>
+    <div className="App" >
+      <center>     
+        <MDBCard>
+          <br></br>
+        <h1>LoL NA Ladder Rankings</h1>
+        <br>
+        </br>
+      <ul className="list">
+        {(products && products.length > 0) ? (
+          products.map(product => renderProduct(product))
+        ) : (
+          <p>No products found</p>
+        )}
+      </ul>
+      </MDBCard>
+      </center>
+    </div>
+    </MDBContainer>
+  );
+}
+
+export default Diamond;
